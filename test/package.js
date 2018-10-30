80fac697d3d654b058f7c9ee4b5793931eb72074
Khen Solomon Lethil <khensolomon@gmail.com>
2018-10-30 10:04:39 +0100

    remove number format

--- test/package.js
+++ test/package.js
@@ -9,8 +9,8 @@ let query = process.argv.slice(2)[0];
 // query = '27,000,000.00';
 // query = '၁,၂၀၀၀၀၀.၂ဝ';
 // query = '၂၇၀၀';
-// query = '၂,၇၀၀';
-query = '5.23e+8';
+query = '၂,၇၀၀';
+// query = '5.23e+8';
 
 let raw = notation.get(query);
 fs.writeFile("./test/package.json", JSON.stringify(raw, null, 2), 'utf8', e=>console.log(e || "...done"));
