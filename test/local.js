ad8b3df18e35ebf93b86c5e3c92ac083bb43a4fd
Khen Solomon Lethil <khensolomon@gmail.com>
2021-03-28 06:43:55 +0200

    add local.js

--- test/local.js
+++ test/local.js
@@ -3,11 +3,13 @@
 import fs from 'fs';
 import notation from '../lib/index.js';
 
+const outFile = "./test/local.json";
+
 // let query = process.argv.slice(2)[0];
 // query = '၁,၂၀၀၀၀၀.၂ဝ';
 
 var raw = [];
-[12345678,2700,'၂,၇၀၀','27,000,000.00','5.23e+8'].forEach(query=>raw.push(notation.get(query)))
+[12345678,2700,'၂,၇၀၀','27,000,000.00','5.23e+8'].forEach(query=>raw.push(notation.get(query)));
 // ['27,000,000.00'].forEach(query=>raw.push(notation.get(query)))
 
-fs.writeFile("./test/mod.json", JSON.stringify(raw, null, 2), 'utf8', e=>console.log(e || "...done"));
+fs.writeFile(outFile, JSON.stringify(raw, null, 2), 'utf8', e=>console.log(e || "...done"));
